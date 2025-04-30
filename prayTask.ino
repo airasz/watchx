ae3d2d99d8a4f60ae47a975f080b21cd2443a255
airasz <rirulez.mz@gmail.com>
2025-04-30 10:50:11 +0700

    work sprintf

--- prayTask.ino
+++ prayTask.ino
@@ -1,249 +1,228 @@
 #include "datajadwal.h"
 
-void getPraytaskID(void)
-{
+void getPraytaskID(void) {
 
-        // hh = tnow.hour;
-        // mm = tnow.minute;
-        formating(); // datajadwal.ino
-        if (hh < jsu || (hh == jsu && mm < msu))
-        { // jam 24 - shubuh
-                ID_ = 0;
-                tomorow = 0;
-                // if (updateDPray == 1)
-                // {
-                //         updateDPray = 2;
-                //         getPraytaskID();
-                // }
-        }
-        else if (hh > jis || (hh == jis && mm >= mis))
-        { // jam isya - 24
-                ID_ = 0;
-                tomorow = 1;
+  // hh = tnow.hour;
+  // mm = tnow.minute;
+  formating();                                // datajadwal.ino
+  if (hh < jsu || (hh == jsu && mm < msu)) {  // jam 24 - shubuh
+    ID_ = 0;
+    tomorow = 0;
+    // if (updateDPray == 1)
+    // {
+    //         updateDPray = 2;
+    //         getPraytaskID();
+    // }
+  } else if (hh > jis || (hh == jis && mm >= mis)) {  // jam isya - 24
+    ID_ = 0;
+    tomorow = 1;
 
-                // updateDPray = true;
-                // if (updateDPray == 0)
-                // {
-                //         updateDPray = 1;
-                //         getPraytaskID();
-                // }
-        }
-        else if ((hh < jdz || (hh == jdz && mm < mdz)) && (hh > jsu || (hh == jsu && mm >= msu)))
-        {
-                ID_ = 1;
-        }
-        else if ((hh < jas || (hh == jas && mm < mas)) && (hh > jdz || (hh == jdz && mm >= mdz)))
-        {
-                ID_ = 2;
-        }
+    // updateDPray = true;
+    // if (updateDPray == 0)
+    // {
+    //         updateDPray = 1;
+    //         getPraytaskID();
+    // }
+  } else if ((hh < jdz || (hh == jdz && mm < mdz)) && (hh > jsu || (hh == jsu && mm >= msu))) {
+    ID_ = 1;
+  } else if ((hh < jas || (hh == jas && mm < mas)) && (hh > jdz || (hh == jdz && mm >= mdz))) {
+    ID_ = 2;
+  }
 
-        else if ((hh < jmag || (hh == jmag && mm < mmag)) && (hh > jas || (hh == jas && mm >= mas)))
-        {
-                ID_ = 3;
-        }
-        else if ((hh < jis || (hh == jis && mm < mis)) && (hh > jmag || (hh == jmag && mm >= mmag)))
-        {
-                ID_ = 4;
-        }
-        // Serial.printf("hh %d\n ", hh);
-        // Serial.printf("jdz %d\n ", jdz);
-        // Serial.printf("pray ID %d\n ", ID_);
+  else if ((hh < jmag || (hh == jmag && mm < mmag)) && (hh > jas || (hh == jas && mm >= mas))) {
+    ID_ = 3;
+  } else if ((hh < jis || (hh == jis && mm < mis)) && (hh > jmag || (hh == jmag && mm >= mmag))) {
+    ID_ = 4;
+  }
+  // Serial.printf("hh %d\n ", hh);
+  // Serial.printf("jdz %d\n ", jdz);
+  // Serial.printf("pray ID %d\n ", ID_);
 }
-void showDayPray(void)
-{
-        getPraytaskID();
-        display.fillScreen(GxEPD_WHITE);
-        // display.setTextColor(TFT_WHITE, TFT_BLUE);
-        display.setTextColor(GxEPD_BLACK);
-        // display.setTextFont(2);
-        display.setFont(&FreeSans9pt7b);
-        display.setCursor(0, 26);
-        display.print("Jadwal solat ");
-        int maxday = MaxDate[imnt - 1]; // get maximun day in current month
+void showDayPray(void) {
+  getPraytaskID();
+  //   display.fillScreen(GxEPD_WHITE);
+  //   display.setTextColor(GxEPD_BLACK);
+  //   display.setFont(&FreeSans9pt7b);
+  //   display.setCursor(0, 26);
+  //   display.print("Jadwal solat ");
+  printText2(0, 26, "Jadwal solat ", false);
+  int maxday = MaxDate[imnt - 1];  // get maximun day in current month
 
-        String datej;
-        if (tomorow)
-        {
-                datej = "besok, ";
-                // print next day
-                if (iday + 1 > maxday)
-                {
-                        imnt += 1;
-                        (imnt > 11) && (imnt = 1); // over december  back to jan
-                        iday = 1;
-                        datej += iday;
-                }
-                else
-                {
-                        datej += iday + 1;
-                }
-        }
-        else
-        {
-                datej = "hari ini, ";
-                // iday += 1;
-                datej += iday;
-        }
-        Serial.printf(" iday : %d \n", iday);
-        datej += " ";
-        datej += bulan[imnt - 1];
-        display.print(datej);
-        String sdata = "";
-        display.setTextColor(GxEPD_BLACK);
-        int r = random(10);
+  String datej;
+  if (tomorow) {
+    datej = "besok, ";
+    // print next day
+    if (iday + 1 > maxday) {
+      imnt += 1;
+      (imnt > 11) && (imnt = 1);  // over december  back to jan
+      iday = 1;
+      datej += iday;
+    } else {
+      datej += iday + 1;
+    }
+  } else {
+    datej = "hari ini, ";
+    // iday += 1;
+    datej += iday;
+  }
+  Serial.printf(" iday : %d \n", iday);
+  datej += " ";
+  datej += bulan[imnt - 1];
+  //   display.print(datej);
+  printText(datej, false);
+  String sdata = "";
+  display.setTextColor(GxEPD_BLACK);
+  int r = random(10);
 
-        while (r == 8 || r == 5) // avoid color no 5&8
-                r = random(10);
-        // int color_ = COLOR_MEDIUM[r];
-        // Serial.printf(" r : %d \n", r);
-        display.setCursor(0, 60);
-        for (uint8_t i = 0; i < 5; i++)
-        {
-                sdata = getDailyJWS(i);
-                display.setTextColor(GxEPD_BLACK);
-                // display.setFont(FF14);
-                display.setFont(&FreeSans9pt7b);
-                if (i == ID_)
-                        display.print("> " + sdata + "\n");
-        }
-        close_incoming_event();
+  while (r == 8 || r == 5)  // avoid color no 5&8
+    r = random(10);
+  // int color_ = COLOR_MEDIUM[r];
+  // Serial.printf(" r : %d \n", r);
+  display.setCursor(0, 60);
+  for (uint8_t i = 0; i < 5; i++) {
+    sdata = getDailyJWS(i);
+    display.setTextColor(GxEPD_BLACK);
+    // display.setFont(FF14);
+    display.setFont(&FreeSans9pt7b);
+    if (i == ID_)
+      printText("> " + sdata + "\n", false);
+    // display.print("> " + sdata + "\n");
+  }
+  close_incoming_event();
 }
 int t;
 bool critical = false;
-void close_incoming_event()
-{ // close_incoming_event code here
-        display.setTextColor(GxEPD_BLACK);
-        display.setCursor(0, 200);
-        // display.setFont(FF18);
-        display.setFont(&FreeSans9pt7b);
+void close_incoming_event() {  // close_incoming_event code here
+  display.setTextColor(GxEPD_BLACK);
+  display.setCursor(0, 200);
+  // display.setFont(FF18);
+  display.setFont(&FreeSans9pt7b);
 
-        for (size_t i = 0; i < 4; i++)
-        {
-                if (hh == dPraySche[i][0])
-                {
-                        if (mm < dPraySche[i][1])
-                        {
-                                t = dPraySche[i][1] - mm;
-                                display.printf("%d menit\nmenjelang %s", t, prayName[i][0]);
-                                critical = (t < 15 && t > 0) ? 1 : 0;
-                        }
-                        else
-                        {
-                                critical = 0;
-                        }
+  for (size_t i = 0; i < 4; i++) {
+    if (hh == dPraySche[i][0]) {
+      if (mm < dPraySche[i][1]) {
+        t = dPraySche[i][1] - mm;
+        // display.printf("%d menit\nmenjelang %s", t, prayName[i][0]);
+        char buff[40];
+        sprintf(buff, "%d menit\nmenjelang %s", t, prayName[i][0]);
+        printText(buff, false);
+        critical = (t < 15 && t > 0) ? 1 : 0;
+      } else {
+        critical = 0;
+      }
 
-                } // else{t=0;}
-                if ((dPraySche[i][0] - hh == 1) && (mm > dPraySche[i][1]))
-                {
-                        t = dPraySche[i][1] + (60 - mm);
-                        display.printf("%d menit\nmenjelang %s", t, prayName[i][0]);
-                        critical = (t < 15 && t > 0) ? 1 : 0;
-                }
-        }
+    }  // else{t=0;}
+    if ((dPraySche[i][0] - hh == 1) && (mm > dPraySche[i][1])) {
+      t = dPraySche[i][1] + (60 - mm);
+      //       display.printf("%d menit\nmenjelang %s", t, prayName[i][0]);
+      char buff[40];
+      sprintf(buff, "%d menit\nmenjelang %s", t, prayName[i][0]);
+      printText(buff, false);
+      critical = (t < 15 && t > 0) ? 1 : 0;
+    }
+  }
 
-        Serial.print("t = ");
-        Serial.println(t);
-        Serial.print("critical = ");
-        Serial.println(critical);
+  Serial.print("t = ");
+  Serial.println(t);
+  Serial.print("critical = ");
+  Serial.println(critical);
 }
-void dump()
-{
+void dump() {
 
-        if (hh == jdz)
-        {
-                if (mm < mdz)
-                {
-                        t = mdz - mm;
-                        display.printf("%d menit\nmenjelang dzuhur", t);
-                        critical = (t < 15 && t > 0) ? 1 : 0;
-                }
-                else
-                {
-                        critical = 0;
-                }
+  if (hh == jdz) {
+    if (mm < mdz) {
+      t = mdz - mm;
+      //       display.printf("%d menit\nmenjelang dzuhur", t);
+      //       String cie = sprintf("%d menit\nmenjelang dzuhur", t);
+      //       printText(cie, false);
+      char buff[40];
+      sprintf(buff, "%d menit\nmenjelang dzuhur", t);
+      printText(buff, false);
+      critical = (t < 15 && t > 0) ? 1 : 0;
+    } else {
+      critical = 0;
+    }
 
-        } // else{t=0;}
-        if ((jdz - hh == 1) && (mm > mdz))
-        {
-                t = mdz + (60 - mm);
-                display.printf("%d menit\nmenjelang dzuhur", t);
-                critical = (t < 15 && t > 0) ? 1 : 0;
-        } // else{t=0;}
-        if (hh == jas)
-        {
-                if (mm < mas)
-                {
-                        t = mas - mm;
-                        display.printf("%d menit\nmenjelang azhar", t);
-                        critical = (t < 15 && t > 0) ? 1 : 0;
-                }
-                else
-                {
-                        critical = 0;
-                }
-        } // else{t=0;}
-        if ((jas - hh == 1) && (mm > mas))
-        {
-                t = mas + (60 - mm);
-                display.printf("%d menit\nmenjelang azhar", t);
-                critical = (t < 15 && t > 0) ? 1 : 0;
-        } // else{t=0;}
-        if (hh == jmag)
-        {
-                if (mm < mmag)
-                {
-                        t = mmag - mm;
-                        display.printf("%d menit\nmenjelang maghrib", t);
-                        critical = (t < 15 && t > 0) ? 1 : 0;
-                }
-                else
-                {
-                        critical = 0;
-                }
-        } // else{t=0;}
-        if ((jmag - hh == 1) && (mm > mmag))
-        {
-                t = mmag + (60 - mm);
-                display.printf("%d menit\nmenjelang maghrib", t);
-                critical = (t < 15 && t > 0) ? 1 : 0;
-        } // else{t=0;}
-        if (hh == jis)
-        {
-                if (mm < mis)
-                {
-                        t = mis - mm;
-                        display.printf("%d menit\nmenjelang isya'", t);
-                        critical = (t < 15 && t > 0) ? 1 : 0;
-                }
-                else
-                {
-                        critical = 0;
-                }
-        } // else{t=0;}
-        if ((jis - hh == 1) && (mm > mis))
-        {
-                t = mis + (60 - mm);
-                display.printf("%d menit\nmenjelang isya'", t);
-                critical = (t < 15 && t > 0) ? 1 : 0;
-        } // else{t=0;}
-        if (hh == jsu)
-        {
-                if (mm < msu)
-                {
-                        t = msu - mm;
-                        display.printf("%d menit\nmenjelang shubuh", t);
-                        critical = (t < 15 && t > 0) ? 1 : 0;
-                }
-                else
-                {
-                        critical = 0;
-                }
-        } // else{t=0;}
-        if ((jsu - hh == 1) && (mm > msu))
-        {
-                t = msu + (60 - mm);
-                display.printf("%d menit\nmenjelang shubuh", t);
-                critical = (t < 15 && t > 0) ? 1 : 0;
+  }  // else{t=0;}
+  if ((jdz - hh == 1) && (mm > mdz)) {
+    t = mdz + (60 - mm);
+    char buff[40];
+    sprintf(buff, "%d menit\nmenjelang dzuhur", t);
+    printText(buff, false);
+    critical = (t < 15 && t > 0) ? 1 : 0;
+  }  // else{t=0;}
+  if (hh == jas) {
+    if (mm < mas) {
+      t = mas - mm;
+      //       display.printf("%d menit\nmenjelang azhar", t);
+      char buff[40];
+      sprintf(buff, "%d menit\nmenjelang azhar", t);
+      printText(buff, false);
+      critical = (t < 15 && t > 0) ? 1 : 0;
+    } else {
+      critical = 0;
+    }
+  }  // else{t=0;}
+  if ((jas - hh == 1) && (mm > mas)) {
+    t = mas + (60 - mm);
+    char buff[40];
+    sprintf(buff, "%d menit\nmenjelang azhar", t);
+    printText(buff, false);
+    critical = (t < 15 && t > 0) ? 1 : 0;
+  }  // else{t=0;}
+  if (hh == jmag) {
+    if (mm < mmag) {
+      t = mmag - mm;
+      char buff[40];
+      sprintf(buff, "%d menit\nmenjelang maghrib", t);
+      printText(buff, false);
+      critical = (t < 15 && t > 0) ? 1 : 0;
+    } else {
+      critical = 0;
+    }
+  }  // else{t=0;}
+  if ((jmag - hh == 1) && (mm > mmag)) {
+    t = mmag + (60 - mm);
+    char buff[40];
+    sprintf(buff, "%d menit\nmenjelang maghrib", t);
+    printText(buff, false);
+    critical = (t < 15 && t > 0) ? 1 : 0;
+  }  // else{t=0;}
+  if (hh == jis) {
+    if (mm < mis) {
+      t = mis - mm;
+      char buff[40];
+      sprintf(buff, "%d menit\nmenjelang isya'", t);
+      printText(buff, false);
+      critical = (t < 15 && t > 0) ? 1 : 0;
+    } else {
+      critical = 0;
+    }
+  }  // else{t=0;}
+  if ((jis - hh == 1) && (mm > mis)) {
+    t = mis + (60 - mm);
+    char buff[40];
+    sprintf(buff, "%d menit\nmenjelang isya'", t);
+    printText(buff, false);
+    critical = (t < 15 && t > 0) ? 1 : 0;
+  }  // else{t=0;}
+  if (hh == jsu) {
+    if (mm < msu) {
+      t = msu - mm;
+      char buff[40];
+      sprintf(buff, "%d menit\nmenjelang shubuh", t);
+      printText(buff, false);
+      critical = (t < 15 && t > 0) ? 1 : 0;
+    } else {
+      critical = 0;
+    }
+  }  // else{t=0;}
+  if ((jsu - hh == 1) && (mm > msu)) {
+    t = msu + (60 - mm);
+    char buff[40];
+    sprintf(buff, "%d menit\nmenjelang shubuh", t);
+    printText(buff, false);
+    critical = (t < 15 && t > 0) ? 1 : 0;
 
-        } // else{t=0;}
+  }  // else{t=0;}
 }
